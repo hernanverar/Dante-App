e31d3b53a69067511e32fc52235c022bebe7c33b
Hernan verar <hernanverar@gmail.com>
2024-06-13 19:50:38 -0700

    This commit adds images, to our assets, added navbar, and nav.css

--- dante/src/App.js
+++ dante/src/App.js
@@ -2,16 +2,19 @@ import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import About from "./about/About";
 import Background from "./background/Background";
+import Nav from "./nav/Nav";
 
 const App = () => {
   return (
+    <div> 
     <Router>
+      <Nav />
       <Background />
-
       <Routes>
         <Route path="/" element={<About />} />
       </Routes>
     </Router>
+    </div>
   );
 };
 
---
+++ dante/src/nav/Nav.js
@@ -0,0 +1,66 @@
+import { Link, useLocation } from "react-router-dom";
+import astronautHelmet from "../assets/astronaut-helmet.png";
+import deadEye from "../assets/dead-eye.png";
+import stack from "../assets/stack.png";
+import envelope from "../assets/envelope.png";
+
+export default function Nav() {
+  const location = useLocation();
+
+  const getNavPositionClass = () => {
+    switch (location.pathname) {
+      case "/":
+        return "nav-about";
+      default:
+        return "";
+    }
+  };
+
+  const getPageTitle = () => {
+    switch (location.pathname) {
+      case "/":
+        return "INICIO";
+      case "/requisitos":
+        return "REQUISITOS";
+      case "/projects":
+        return "PROJECTS";
+      case "/contact":
+        return "CONTACT";
+      default:
+        return "";
+    }
+  };
+
+  const navPositionClass = getNavPositionClass();
+  const pageTitle = getPageTitle();
+
+  const isCurrentPage = (navClass) => {
+    return navClass === navPositionClass;
+  };
+
+  const renderNavLink = (to, imgSrc, altText, navClass) => {
+    const isCurrent = isCurrentPage(navClass);
+    const linkClass = isCurrent ? "nav-link current" : "nav-link";
+
+    return (
+      <Link to={to} className={linkClass}>
+        <img src={imgSrc} alt={altText} />
+        {isCurrent && <h1 className="page-title">{pageTitle}</h1>}
+      </Link>
+    );
+  };
+
+  return (
+    <nav className={`nav ${navPositionClass}`}>
+      {renderNavLink(
+        "/",
+        astronautHelmet,
+        "astronaut helmet icon",
+        "nav-about"
+      )}
+      {renderNavLink("/requisitos", deadEye, "deadEye icon", "nav-reqisitos")}
+      {renderNavLink("/projects", stack, "stack icon", "nav-projects")}
+      {renderNavLink("/contact", envelope, "envelope icon", "nav-contact")}
+    </nav>
+  );
+}
